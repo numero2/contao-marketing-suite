f4bb47f98536234dfb653ef133dae52e38f1b9c7
Benny Born <benny.born@numero2.de>
2025-10-30 09:18:12 +0100

    fix

--- src/Resources/contao/library/MarketingSuite/Helper/Domain.php
+++ src/Resources/contao/library/MarketingSuite/Helper/Domain.php
@@ -6,12 +6,13 @@
  * @author    Benny Born <benny.born@numero2.de>
  * @author    Michael Bösherz <michael.boesherz@numero2.de>
  * @license   Commercial
- * @copyright Copyright (c) 2024, numero2 - Agentur für digitales Marketing GbR
+ * @copyright Copyright (c) 2025, numero2 - Agentur für digitales Marketing GbR
  */
 
 
 namespace numero2\MarketingSuite\Helper;
 
+use Contao\System;
 use Pdp\Parser;
 use Pdp\PublicSuffixListManager;
 use Pdp\Rules;
@@ -32,13 +33,13 @@ class Domain {
         // jeremykendall/php-domain-parser ^3.0
         if( class_exists('\Pdp\PublicSuffixListManager') ) {
 
-            $oManager = NULL;
+            $oManager = null;
             $oManager = new PublicSuffixListManager();
 
-            $oParser = NULL;
+            $oParser = null;
             $oParser = new Parser($oManager->getList());
 
-            $oURL = NULL;
+            $oURL = null;
             $oURL = $oParser->parseUrl($sDomain);
 
             return $oURL->host->registerableDomain;
@@ -46,15 +47,17 @@ class Domain {
         // jeremykendall/php-domain-parser ^5.6
         } else if( class_exists('\Pdp\Domain') ) {
 
-            $oRules = NULL;
-            $oRules = Rules::fromPath(TL_ROOT . '/vendor/numero2/contao-marketing-suite/src/Resources/vendor/publicsuffix/public_suffix_list.dat');
+            $root = System::getContainer()->getParameter('kernel.project_dir');
 
-            $oDomain = NULL;
+            $oRules = null;
+            $oRules = Rules::fromPath($root . '/vendor/numero2/contao-marketing-suite/src/Resources/vendor/publicsuffix/public_suffix_list.dat');
+
+            $oDomain = null;
             $oDomain = $oRules->resolve($sDomain);
 
-            return $oDomain->getRegistrableDomain();
+            return $oDomain->registrableDomain()->value();
         }
 
-        return NULL;
+        return null;
     }
 }
